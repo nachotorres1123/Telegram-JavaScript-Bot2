c32bff024a1c070bbc97f3e3b2fdd1264b0b0f79
nacho_torres_1123@hotmail.com <43484201+nachotorres1123@users.noreply.github.com>
2023-08-13 16:18:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ const { Bot, GrammyError, HttpError } = require("grammy");
 const { autoQuote } = require("@roziscoding/grammy-autoquote");
 const fs = require("fs");
 const path = require("path");
+const { Configuration, OpenAIApi } = require("openai"); // Importa la biblioteca de OpenAI
 
 if (fs.existsSync(".env")) {
   require("dotenv").config();
 }
 
 const botToken = process.env.BOT_TOKEN;
+const openaiApiKey = "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 "; // Reemplaza con tu clave de API de OpenAI
+
 if (!botToken) {
   throw new Error("BOT_TOKEN is not set in environment variables! Exiting...");
 }
+
+const openaiConfiguration = new Configuration({
+    apiKey: openaiApiKey,
+});
+
+const openai = new OpenAIApi(openaiConfiguration);
 
 async function start() {
   const bot = new Bot(botToken);
@@ -24,13 +33,13 @@ async function start() {
   for (const file of commandFiles) {
     const command = require(path.join(commandFilesDir, file));
     bot.command(command.name, async (ctx) => {
-      await command.handler(ctx);
+      await command.handler(ctx, openai); // Pasa la instancia de OpenAI a los manejadores de comandos
     });
 
     if (command.alias) {
       for (const alias of command.alias) {
         bot.command(alias, async (ctx) => {
-          await command.handler(ctx);
+          await command.handler(ctx, openai); // Pasa la instancia de OpenAI a los manejadores de comandos
         });
       }
     }
